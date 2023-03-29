8d4ce3529492e7a6e5f8e7c025ce28e4007f2944
Thomas S <thomas.swanson107@gmail.com>
2023-03-28 23:43:59 -0700

    fix win condition actually works now

--- game.js
+++ game.js
@@ -10,6 +10,7 @@ let player_choice = String(prompt("Rock, Paper, or Scissors: "));
 
 console.log("player play: " + player_choice);
 
+
 let player_play = player_choice.toLowerCase();
 
 if (player_play === "rock"){
@@ -32,16 +33,16 @@ if (cpu_choice === 0){
 
 console.log("cpu play: " + cpu_play)
 
-if (player_play > cpu_play && cpu_play != 2){
+if (player_play === 0 && cpu_choice === 2){
+    console.log("Player Wins!!");
+} else if (player_play === 2 && cpu_choice === 0){
+    console.log("CPU Wins!!");
+} else if (player_play > cpu_choice){
     console.log("Player Wins!");
-} else if (player_play < cpu_play && player_play != 2){
+} else if (player_play < cpu_choice){
     console.log("CPU Wins!");
-} else if (player_play === cpu_play){
+} else if (player_play === cpu_choice){
     console.log("Tie!");
-} else if (player_play === 2 && cpu_play === 0){
-    console.log("CPU Wins!");
-} else if (player_player === 0 && cpu_play === 2){
-    console.log("Player Wins!");
 }
 
 
