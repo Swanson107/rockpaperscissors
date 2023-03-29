fc2f9270a87579f4d9a60ca8c2f20fd100aba4c9
Thomas S <thomas.swanson107@gmail.com>
2023-03-28 23:48:57 -0700

    put game into function and added loop

--- game.js
+++ game.js
@@ -1,49 +1,56 @@
 
 
+function game(){
 
-function getComputerChoice() {
-    let choice = Math.floor(Math.random() * 3)
-    return choice;
+    function getComputerChoice() {
+        let choice = Math.floor(Math.random() * 3)
+        return choice;
+    }
+
+    let player_choice = String(prompt("Rock, Paper, or Scissors: "));
+
+    console.log("player play: " + player_choice);
+
+
+    let player_play = player_choice.toLowerCase();
+
+    if (player_play === "rock"){
+        player_play = 0;
+    } else if (player_play === "paper"){
+        player_play = 1;
+    } else if (player_play === "scissors" || player_play === "scissor"){
+        player_play = 2;
+    }
+
+    let cpu_choice = getComputerChoice();
+
+    if (cpu_choice === 0){
+        cpu_play = "rock";
+    } else if (cpu_choice === 1){
+        cpu_play = "paper";
+    } else if (cpu_choice === 2){
+        cpu_play = "scissors";
+    }
+
+    console.log("cpu play: " + cpu_play)
+
+    if (player_play === 0 && cpu_choice === 2){
+        console.log("Player Wins!!");
+    } else if (player_play === 2 && cpu_choice === 0){
+        console.log("CPU Wins!!");
+    } else if (player_play > cpu_choice){
+        console.log("Player Wins!");
+    } else if (player_play < cpu_choice){
+        console.log("CPU Wins!");
+    } else if (player_play === cpu_choice){
+        console.log("Tie!");
+    }
+
+
 }
 
-let player_choice = String(prompt("Rock, Paper, or Scissors: "));
+let rounds = parseInt(prompt("How many rounds: "));
 
-console.log("player play: " + player_choice);
-
-
-let player_play = player_choice.toLowerCase();
-
-if (player_play === "rock"){
-    player_play = 0;
-} else if (player_play === "paper"){
-    player_play = 1;
-} else if (player_play === "scissors" || player_play === "scissor"){
-    player_play = 2;
+for (i = 0; i < rounds; i++){
+    game();
 }
-
-let cpu_choice = getComputerChoice();
-
-if (cpu_choice === 0){
-    cpu_play = "rock";
-} else if (cpu_choice === 1){
-    cpu_play = "paper";
-} else if (cpu_choice === 2){
-    cpu_play = "scissors";
-}
-
-console.log("cpu play: " + cpu_play)
-
-if (player_play === 0 && cpu_choice === 2){
-    console.log("Player Wins!!");
-} else if (player_play === 2 && cpu_choice === 0){
-    console.log("CPU Wins!!");
-} else if (player_play > cpu_choice){
-    console.log("Player Wins!");
-} else if (player_play < cpu_choice){
-    console.log("CPU Wins!");
-} else if (player_play === cpu_choice){
-    console.log("Tie!");
-}
-
-
-
