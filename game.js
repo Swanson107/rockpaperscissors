61fa0fa03a8c9bd22d46d1648e876f7d53c16229
Thomas S <thomas.swanson107@gmail.com>
2023-03-28 23:25:08 -0700

    Add win/lose conditions

--- game.js
+++ game.js
@@ -10,8 +10,15 @@ let player_choice = String(prompt("Rock, Paper, or Scissors: "));
 
 console.log("player play: " + player_choice);
 
-let player_play = player_choice.toLowerCase;
-console.log(player_play)
+let player_play = player_choice.toLowerCase();
+
+if (player_play === "rock"){
+    player_play = 0;
+} else if (player_play === "paper"){
+    player_play = 1;
+} else if (player_play === "scissors" || player_play === "scissor"){
+    player_play = 2;
+}
 
 let cpu_choice = getComputerChoice();
 
@@ -24,6 +31,18 @@ if (cpu_choice === 0){
 }
 
 console.log("cpu play: " + cpu_play)
+
+if (player_play > cpu_play && cpu_play != 2){
+    console.log("Player Wins!");
+} else if (player_play < cpu_play && player_play != 2){
+    console.log("CPU Wins!");
+} else if (player_play === cpu_play){
+    console.log("Tie!");
+} else if (player_play === 2 && cpu_play === 0){
+    console.log("CPU Wins!");
+} else if (player_player === 0 && cpu_play === 2){
+    console.log("Player Wins!");
+}
 
 
 
