1115de4525b246cf4222e6fb0d702957b98919f8
Thomas S <thomas.swanson107@gmail.com>
2023-03-31 16:48:28 -0700

    Add scoring system and win/lose

--- game.js
+++ game.js
@@ -1,3 +1,11 @@
+// initialize vars
+let rounds = 0;
+let playerScore = 0;
+let cpuScore = 0;
+
+document.getElementById('player-score').textContent = 'Player score: 0';
+document.getElementById('cpu-score').textContent = 'CPU score: 0';
+
 
 function game(player_play){
     console.log(player_play);
@@ -64,13 +72,17 @@ function setRounds() {
 }
 
 
-function eventHandler() {
+function updateInput() {
     if (document.getElementById('input').value !== '') {
         document.getElementById('show-rounds').textContent = document.getElementById('input').value;
     }
 }
 
 function getRounds() {
+    playerScore = 0;
+    cpuScore = 0;
+    document.getElementById('player-score').textContent = 'Player score: 0'
+    document.getElementById('cpu-score').textContent = 'CPU score: 0'
     rounds = document.getElementById('input').value;
     document.getElementById('round-total').textContent = `Rounds: ${rounds} / ${rounds}`;
     return rounds;
@@ -82,15 +94,19 @@ function getTotalRounds() {
     return totalRounds;
 }
 
-let rounds = 0;
+
 
 function roundDec() {
     const roundsTotal = getTotalRounds();
     console.log("Total Rounds: " + roundsTotal);
     console.log("Rounds before decrement: " + rounds);
+
     rounds -= 1;
     console.log("Rounds: " + rounds);
     document.getElementById('round-total').textContent = `Rounds: ${rounds} / ${getTotalRounds()}`;
+    if (rounds <= 0){
+        gameOver();
+    }
 }
 
 function changeRound(rounds) {
@@ -98,6 +114,33 @@ function changeRound(rounds) {
     return rounds;
 }
 
+function gameOver() {
+    if (playerScore > cpuScore){
+        console.log("Player Winner");
+        document.getElementById('select-text').textContent = "Player wins the game!";
+    } else if (cpuScore > playerScore) {
+        console.log("CPU Winner");
+        document.getElementById('select-text').textContent = "CPU wins the game!";
+    } else if (cpuScore === playerScore) {
+        console.log("Tie. No Winner");
+        document.getElementById('select-text').textContent = "Tie! No winner this time";
+    }
+    document.getElementById('round-total').textContent = 'Game Over';
+    document.getElementById('input-field').style.display = 'flex';
+    console.log("Game Over");
 
+}
+
+function scoreCount() {
+    console.log("scoreCount func start")
+    var winner = document.getElementById('resolution').textContent;
+    if (winner == 'Player Wins!') {
+        playerScore += 1;
+        document.getElementById('player-score').textContent = `Player score: ${playerScore}`;
+    } else if (winner == 'CPU Wins!') {
+        cpuScore += 1;
+        document.getElementById('cpu-score').textContent = `CPU score: ${cpuScore}`;
+    }
+}
 
 
